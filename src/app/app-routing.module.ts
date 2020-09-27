382b712058b6152585ab336a0e957303487f33da
etsec-cloud <etienne.secondini@hotmail.fr>
2020-09-27 23:23:36 +0200

    push2nd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { ConnexionComponent } from "./component/account/connexion/connexion.component";
 import { InscriptionComponent } from "./component/account/inscription/inscription.component";
+import { BracketComponent } from "./component/bracket/bracket.component";
 
 const routes: Routes = [
   { path: "connexion", component: ConnexionComponent },
   { path: "inscription", component: InscriptionComponent },
+  { path: "bracket", component: BracketComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
