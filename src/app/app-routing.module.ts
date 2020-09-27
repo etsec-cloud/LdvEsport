4b58d4a5c10013ecffcecb343f54c3c6da8c5e2f
etsec-cloud <etienne.secondini@hotmail.fr>
2020-09-27 23:44:34 +0200

    merging

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from "@angular/router";
 import { ConnexionComponent } from "./component/account/connexion/connexion.component";
 import { InscriptionComponent } from "./component/account/inscription/inscription.component";
 import { BracketComponent } from "./component/bracket/bracket.component";
+import { HomeComponent } from "./component/home/home.component";
 
 const routes: Routes = [
+  { path: "", component: HomeComponent },
   { path: "connexion", component: ConnexionComponent },
   { path: "inscription", component: InscriptionComponent },
   { path: "bracket", component: BracketComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ConnexionComponent } from "./component/account/connexion/connexion.component";
 import { InscriptionComponent } from "./component/account/inscription/inscription.component";
 import { BracketComponent } from "./component/bracket/bracket.component";
+import { HomeComponent } from "./component/home/home.component";
 import { MatchComponent } from "./component/match/match.component";
+import { NavbarComponent } from "./component/navbar/navbar.component";
+import { TableComponent } from "./component/table/table.component";
 
 @NgModule({
   declarations: [
@@ -14,9 +26,26 @@ import { MatchComponent } from "./component/match/match.component";
     ConnexionComponent,
     InscriptionComponent,
     MatchComponent,
-    BracketComponent
+    BracketComponent,
+    HomeComponent,
+    NavbarComponent,
+    TableComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatNativeDateModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    MatDatepickerModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
